2f59691e3b019d62c7aa8b7d6d2a37635bff63d5
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-02 11:35:43 +0900

    Create Recursions.cpp

--- C++/Recursions.cpp
+++ C++/Recursions.cpp
@@ -1,5 +1,3 @@
-#include <cstdio>
-
 int fact(int n) {
   return n == 0 ? 1 : n*fact(n-1);
 }
@@ -10,8 +8,4 @@ int fibo(int n) {
 
 int collatz(int n, int a = 0) {
   return n == 1 ? a : (n%2 == 0 ? collatz(n/2,a+1) : collatz(3*n+1,a+1));
-}
-
-int main() {
-  printf("%d\n%d\n%d\n",fact(13),fibo(13),collatz(27));
 }
