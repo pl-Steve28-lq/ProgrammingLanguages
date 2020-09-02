da983cdfaf3110ff43c6ddc5e7612970d500ca4c
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-02 11:36:22 +0900

    Create Collatz.c

--- C/Collatz.c
+++ C/Collatz.c
@@ -1,17 +1,7 @@
-#include <stdio.h>
-
-int collatz(int n);
-int collatz1(int n, int a);
-
-int main(void) {
-  int v;
-  scanf("%d",&v);
-  printf("%d\n", collatz(v));
-  return 0;
-}
 int collatz(int n) {
   return collatz1(n, 0);
 }
+
 int collatz1(int n, int a) {
   if (n == 1) return a;
   else if (n%2 == 0) return collatz1(n/2,a+1);
