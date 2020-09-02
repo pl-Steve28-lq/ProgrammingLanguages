444d1e81acaf25a1ff23f868230b9bf944d06a18
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-02 11:14:48 +0900

    Create ShortRecursions.py

---
+++ Python/ShortRecursions.py
@@ -0,0 +1,3 @@
+f=lambda n:1if n==1or n==2else f(n-1)+f(n-2) #Fibonacci Number Function
+g=lambda x:x*g(x)if x else 1 #Factorial Function
+h=lambda n,a=0:a if n==1else h(3*n+1,a+1)if n%2else h(n/2,a+1) #Length of Collatz Sequence Function
