bc240586588b2acf6439e9cc13d415d70cf8a8ae
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-11 12:07:32 +0900

    Update ShortRecursions.py

--- Python/ShortRecursions.py
+++ Python/ShortRecursions.py
@@ -1,3 +1,14 @@
-f=lambda n:1if n==1or n==2else f(n-1)+f(n-2) #Fibonacci Number Function
-g=lambda x:x*g(x-1)if x else 1 #Factorial Function
-h=lambda n,a=0:a if n==1else h(3*n+1,a+1)if n%2else h(n/2,a+1) #Length of Collatz Sequence Function
+#Fibonacci with O(n^2)
+f=lambda n:1if n==1or n==2else f(n-1)+f(n-2)
+
+#Fibonacci with O(n)
+def F(n):a=[1,1];exec('a.append(a[-1]+a[-2]);'*n);return a[-3]
+
+#Fibonacci with O(n), Extra Smaller
+def F1(n):a,b=0,1;exec('a,b=b,a+b;'*n);return a
+
+#Factorial Function
+g=lambda x:x*g(x-1)if x else 1
+
+#Length of Collatz Sequence Function
+h=lambda n,a=0:a if n==1else h(3*n+1,a+1)if n%2else h(n/2,a+1)
