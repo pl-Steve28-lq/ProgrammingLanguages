602b147cfaff56457cc762fb7eeb5428b7f5f90c
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-09-13 14:08:15 +0900

    Update TableVisualize.lua

--- Lua/TableVisualize.lua
+++ Lua/TableVisualize.lua
@@ -1,4 +1,4 @@
-function Tableprint(a)
+function Tableprint1(a)
   res = ""
   for index, i in pairs(a) do
     if i ~= a[#a] then res = res .. i .. ", "
@@ -16,4 +16,28 @@ function TableStringify(a)
     end
   end
   return "["..res.."]"
+end
+
+-- Function from https://stackoverflow.com/questions/7274380/how-do-i-display-array-elements-in-lua
+function Tableprint(arr, indentLevel)
+    local str = ""
+    local indentStr = "#"
+
+    if(indentLevel == nil) then
+        print(Tableprint(arr, 0))
+        return
+    end
+
+    for i = 0, indentLevel do
+        indentStr = indentStr.."\t"
+    end
+
+    for index,value in pairs(arr) do
+        if type(value) == "table" then
+            str = str..indentStr..index..": \n"..Tableprint(value, (indentLevel + 1))
+        else 
+            str = str..indentStr..index..": "..value.."\n"
+        end
+    end
+    return str
 end
