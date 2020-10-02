eb76857a9fc74870fdb21e51119942655ce4f6f7
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2020-10-02 17:25:32 +0900

    Update SudokuSolver.py

--- Python/SudokuSolver.py
+++ Python/SudokuSolver.py
@@ -1,48 +1,58 @@
-#Return the Block of Sudoku that includes (m, n)
-def Block(sudoku, m, n):
-    a = int(len(sudoku)**.5)
-    M = a*(m//a+1)
-    N = a*(n//a+1)
-    MM = max(0, M-a)
-    NN = max(0, N-a)
-    return list(map(lambda x:x[NN:N], sudoku[MM:M]))
-
-#Return Validation of Sudoku
-def isValid(sudoku, m, n, v):
-    a = sudoku[:]
-    for i in a[m]:
-        if i == v: return False
-    for i in range(len(a)):
-        if a[i][n] == v: return False
-    T = ''.join(map(lambda x: ''.join(map(str, x)), Block(a, m, n))).replace('0','')
-    for i in T:
-        if T.count(i)-1: return False
+class Sudoku:
+    def __init__(self, arr):
+        self.arr = arr
+        self.sol = []
+        self.isEnd = False
     
-    return True
+    #Return the Block of Sudoku that includes (m, n)
+    def Block(self, sudoku, m, n):
+        a = int(len(sudoku)**.5)
+        M = a*(m//a+1)
+        N = a*(n//a+1)
+        MM = max(0, M-a)
+        NN = max(0, N-a)
+        return list(map(lambda x:x[NN:N], sudoku[MM:M]))
 
-#Print Solution of Sudoku, with Backtracking
-def solver(su, Z=0):
-    sudoku = su[:]
+    #Return Validation of Sudoku
+    def isValid(self, sudoku, m, n, v):
+        a = sudoku[:]
+        for i in a[m]:
+            if i == v: return False
+        for i in range(len(a)):
+            if a[i][n] == v: return False
+        T = ''.join(map(lambda x: ''.join(map(str, x)), self.Block(a, m, n))).replace('0','')
+        for i in T:
+            if T.count(i)-1: return False
+        
+        return True
 
-    m = Z // len(sudoku)
-    n = Z % len(sudoku)
+    #Print Solution of Sudoku, with Backtracking
+    def solver(self, sudoku, Z=0): 
+        m = Z // len(sudoku)
+        n = Z % len(sudoku)
 
-    if Z == len(sudoku)**2:
-        for i in sudoku:
-            print(' '.join(map(str, i)))
-        return
-    
-    if sudoku[m][n]: solver(sudoku, Z+1)
-    else:
-        for i in range(1,len(sudoku)+1):
-            #print(sudoku, isValid(sudoku, m, n, i),m,n,i)
-            if isValid(sudoku, m, n, i):
-                sudoku[m][n] = i  
-                solver(sudoku, Z+1)
-                sudoku[m][n] = 0
+        if Z == len(sudoku)**2:
+            self.sol = sudoku[:]
+            self.isEnd = True
+            return 
+        
+        if sudoku[m][n]: self.solver(sudoku, Z+1)
+        else:
+            for i in range(1,len(sudoku)+1):
+                if self.isValid(sudoku, m, n, i):
+                    sudoku[m][n] = i  
+                    self.solver(sudoku, Z+1)
+                    if not self.isEnd: sudoku[m][n] = 0
 
-#Sudoku problem text
-a='''530070000
+    def Solve(self):
+        self.__init__(self.arr)
+        self.solver(self.arr)
+        return self.sol
+
+
+
+if __name__ == "__main__":
+    a='''530070000
 600195000
 098000060
 800060003
@@ -52,7 +62,10 @@ a='''530070000
 000419005
 000080079'''
 
-#Change Sudoku problem into 2d list
-sudoku = list(map(lambda x: list(map(int, list(x))), a.split("\n")))
+    #Change Sudoku problem into 2d list
+    sudoku = Sudoku(list(map(lambda x: list(map(int, list(x))), a.split("\n"))))
 
-solver(sudoku)
+    a = sudoku.Solve()
+    
+    for i in a:
+        print(' '.join(map(str,i)))
