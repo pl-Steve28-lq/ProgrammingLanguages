0b7d19ec7811a8d41595b54acfd17e52fd15a73d
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2021-04-01 15:07:55 +0900

    Update MyQuine.py

--- Python/MyQuine.py
+++ Python/MyQuine.py
@@ -3,3 +3,10 @@ s=r'print(f"s=r\'{s}\';{s}")';print(f"s=r\'{s}\';{s}")
 
 # 2. Longer Quine
 s='Made by';r='Steve28';v='print(f"s=\'{s}\';r=\'{r}\';v=\'{v}\';{v}")';print(f"s=\'{s}\';r=\'{r}\';v=\'{v}\';{v}")
+
+# 3. Multiple Line Quine
+a=['print(f"a={a}")', 'def b():', '    return "Wow, This is Quine code!"', 'print(*a, sep="\\n")']
+print(f"a={a}")
+def b():
+    return "Wow, This is Quine code!"
+print(*a, sep="\n")
