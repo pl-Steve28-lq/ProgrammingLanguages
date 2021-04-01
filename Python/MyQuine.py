2d72cf44dd8a1a61618460fb4a1763c108685831
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2021-04-01 14:49:05 +0900

    Update MyQuine.py

--- Python/MyQuine.py
+++ Python/MyQuine.py
@@ -1,1 +1,5 @@
+# 1. Basic Quine
 s=r'print(f"s=r\'{s}\';{s}")';print(f"s=r\'{s}\';{s}")
+
+# 2. Longer Quine
+s='Made by';r='Steve28';v='print(f"s=\'{s}\';r=\'{r}\';v=\'{v}\';{v}")';print(f"s=\'{s}\';r=\'{r}\';v=\'{v}\';{v}")
