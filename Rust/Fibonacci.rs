0f686b9d810a1fdfc2441b978098685e0c396dcd
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2021-08-11 16:17:43 +0900

    Update Fibonacci.rs

--- Rust/Fibonacci.rs
+++ Rust/Fibonacci.rs
@@ -6,19 +6,17 @@ fn main() {
 }
 
 fn fibo(n: u16) -> u64 {
-  let x = (1, 1, 1, 0);
-  let x = pow(x, n);
-  return x.1
+  pow((1, 1, 1, 0), n).1
 }
 
 fn pow(m: L, n: u16) -> L {
   if n == 0 { return (1, 0, 0, 1) }
   if n == 1 { return m }
-  return mul(pow(m, n/2), pow(m, n/2+n%2))
+  mul(pow(m, n/2), pow(m, n/2+n%2))
 }
 
 fn mul(m: L, n: L) -> L {
   let (a, b, c, d) = m;
   let (e, f, g, h) = n;
-  return (a*e+b*g, a*f+b*h, c*e+d*g, c*f+d*h)
+  (a*e+b*g, a*f+b*h, c*e+d*g, c*f+d*h)
 }
