05c1132ce918dc0f4af78b61c879405469546c02
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2021-08-15 20:45:35 +0900

    Create Fibonacci.exs

---
+++ Elixir/Fibonacci.exs
@@ -0,0 +1,29 @@
+defmodule Main do
+  def main() do
+    print(fibo(15))
+  end
+
+  def fibo(n) do elem pow({1, 1, 1, 0}, n), 1 end
+
+  def pow(x, n) do
+    d = div n, 2
+    case n do
+      0 -> {1, 0, 0, 1}
+      1 -> x
+      n -> mul pow(x, d), pow(x, d + rem(n, 2))
+    end
+  end
+
+  def mul(x, y) do
+    case {x, y} do
+      {{a, b, c, d}, {e, f, g, h}} -> {
+        a*e+b*g, a*f+b*h, c*e+d*g, c*f+d*h
+      }
+      _ -> 0
+    end
+  end
+
+  def print(x) do IO.puts x end
+end
+
+Main.main()
