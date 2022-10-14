ed1722782c2f989f8a5b6cb60885b3dc54adfe31
Steve28 <64412954+pl-Steve28-lq@users.noreply.github.com>
2022-10-14 21:43:00 +0900

    Update Fibonacci.jl

--- Julia/Fibonacci.jl
+++ Julia/Fibonacci.jl
@@ -1,18 +1,19 @@
-function fibo(n)
-  x = [1 1
-      1 0]
-  getindex(pow(x, n), 3)
+const Matrix = Vector{UInt128}
+
+function pow(a :: Matrix, n :: Int) :: Matrix
+    if n == 0 return [1, 0, 0, 1]
+    elseif n == 1 return a end
+    mul(pow(a, n÷2), pow(a, n÷2 + n%2))
 end
 
-function pow(a, n)
-  if n==0 return 1
-  elseif n==1 return a end
-  mul(pow(a, Int8((n-n%2)/2)), pow(a, Int8((n-n%2)/2)+n%2))
+function mul(m :: Matrix, n :: Matrix) :: Matrix
+    a, b, c, d = m
+    e, f, g, h = n
+    [a*e+b*g, a*f+b*h, c*e+d*g, c*f+d*h]
 end
 
-function mul(x, y)
-  a, c, b, d = x
-  e, g, f, h = y
-  [a*e+b*g a*f+b*h
-  c*e+d*g c*f+d*h]
+function fibo(n :: Int) :: UInt128
+    pow(UInt128[1, 1, 1, 0], n)[3]
 end
+
+println(fibo(100))
